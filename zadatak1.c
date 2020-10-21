da4f94dd699ca0bec8e32cc5ab76b19c9963e846
Luka Baranovic <73253333+LukaBaranovic@users.noreply.github.com>
2020-10-21 21:43:23 +0200

    Update and rename Untitled1.c to zadatak1.c

--- zadatak1.c
+++ zadatak1.c
@@ -3,15 +3,11 @@
 #include<malloc.h>
 #define MAX 50
 
-
 typedef struct{
     char ime[25];
     char prezime[25];
     int bodovi;
 }student;
-
-
-
 
 int main(){
 
@@ -24,7 +20,7 @@ int main(){
 
     FILE *dat1;
 
-    dat1=fopen("datoteka243.txt","r");
+    dat1=fopen("datoteka1.txt","r");
 
     if((dat1==NULL)){
         printf("Greska pri ucitavanju datoteka!!!");                       /*provjera otvaranja datoteke*/
@@ -48,31 +44,12 @@ int main(){
 
     for(i=0;i<br;i++){
         printf("%d. student: \n", i+1);                                                 /*ispis*/
-        printf("Ime i prezime: %s %s \n", s[i].ime, s[i].prezime);                      /*stavio sam da je broj bodova 100 jer nije zadano i nije mi bilo*/
-        printf("Broj ostvarenih bodova: %d  \n", s[i].bodovi);                      /*jasno sta da stavim pod relativne bodove*/
-        rel=(float)s[i].bodovi*100/MAX;
+        printf("Ime i prezime: %s %s \n", s[i].ime, s[i].prezime);                      
+        printf("Broj ostvarenih bodova: %d  \n", s[i].bodovi);
         printf("Broj relativnih bodova: %.2f \n\n\n", rel);
     }
-
-
+    
     fclose(dat1);                                                                       /*zatvaranje datoteke*/
-
 
 return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
