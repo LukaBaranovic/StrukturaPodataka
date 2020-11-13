b39205c962d4d0b65ea5c0eaf31da86c19d83418
Luka Baranovic <73253333+LukaBaranovic@users.noreply.github.com>
2020-11-13 13:48:39 +0100

    update

--- Zadatak 5/zadatak5.c
+++ Zadatak 5/zadatak5.c
@@ -11,38 +11,44 @@ struct list {
 	position next;
 };
 
-position create_list();
-void read_from_file(position *);
+position create_list(int);
+void read_from_file(position*);
 void insert(char*, position head);
 void sorted_insert(int, position);
 void show_specific(position);
-
+void show_all_from_file(int, position*);
+int line_counter();
+void make_union(position, position);
+void make_intersection(position, position);
 
 
 int main() {
-	int i, j;
-
-	position* head = create_list();
+	int number = line_counter();
+	position* head = create_list(number);
 	read_from_file(head);
 
-	printf("\n\n 1.lista: ");
-	show_specific(head[0]);
+
+	show_all_from_file(number, head);
+	make_union(head[0], head[1]);
+	make_intersection(head[0], head[1]);
 
 }
 
-position create_list() {
+position create_list(int number) {
 	int i;
 
-	position* head = (position*)malloc(2 * sizeof(position));
-	for (i = 0; i < 2; i++) {
+	position* head = (position*)malloc(number * sizeof(position));
+	for (i = 0; i < number; i++) {
 		position tmp = (position)malloc(sizeof(struct list));
 		tmp->next = NULL;
 		head[i] = tmp;
 	}
-return head;
+
+
+	return head;
 }
 
-void read_from_file(position *head) {
+void read_from_file(position* head) {
 	int x = 0;
 	char str[MAX];
 	FILE* filename = fopen("lista.txt", "r");
@@ -57,24 +63,14 @@ void read_from_file(position *head) {
 	}
 }
 
-void insert(char *str, position head) {
-	int br = 0, compare = NULL;
-	while (sscanf(str, " %d", &br) != NULL) {
-		if (compare != br) {
-			printf(" %d", br);
-			sorted_insert(br, head);
-		}
-		str += 1;
-		compare = br;
+void insert(char* str, position head) {
+	int br = 0, differ = 0;
+	
+	while (sscanf(str, " %d, %n%*[^\n]", &br, &differ) == 1) {
+		str = str + differ;
+		sorted_insert(br, head);
 	}
 }
-
-
-/*ovdje imam pitanje:  jesam li dobro napravio ovo sa skeniranjem varijable i opcenito ovo sve sa funkcijom fgets jer
-ju prvi put korisitm a predlozili ste mi je na proslom satu. Takoder neznam kako preskocit razmake pa mi ispisuje duple brojeve
-tj. ispisuje mi brojeve po 2,3 puta (onoliko koliko razmaka ima). Tu sam napravio neki svoj algoritam koji usporedjuje zadnja 2 
-broja al mislim da takav nacin nije najbolje pa ako znate kako to izvest da mi preskoci taj string na sljedeci broj da mi malo 
-objasnite. Hvala. */
 
 void sorted_insert(int br, position head) {
 	position tmp = (position)malloc(sizeof(struct list));
@@ -83,13 +79,11 @@ void sorted_insert(int br, position head) {
 	while (head->next != NULL && head->next->x >= br) {
 		head = head->next;
 	}
-
 	if (br != head->x) {
 		tmp->x = br;
 		tmp->next = head->next;
 		head->next = tmp;
 	}
-
 }
 
 void show_specific(position head) {
@@ -100,4 +94,77 @@ void show_specific(position head) {
 		printf(" %d", tmp->x);
 	}
 	printf("\n");
+}
+
+void show_all_from_file(int number, position* head) {
+	int i;
+	for (i = 0; i < number; i++) {
+		printf("%d. linija: ", i + 1);
+		show_specific(head[i]);
+	}
+}
+
+int line_counter() {
+	FILE* filename = fopen("lista.txt", "r");
+	int counter = 1;
+	while (!feof(filename)) {
+		if (getc(filename) == '\n')
+			counter++;
+	}
+	rewind(filename);
+	fclose(filename);
+
+	return counter;
+}
+
+void make_union(position list1, position list2) {
+	position list_union = (position)malloc(sizeof(struct list));
+	list_union->next = NULL;
+
+	while (list1->next != NULL && list2->next != NULL) {
+		if (list1->next->x == list2->next->x) {
+			list1 = list1->next;
+			list2 = list2->next;
+			sorted_insert(list1->x, list_union);
+		}
+		else if (list1->next->x > list2->next->x) {
+			list1 = list1->next;
+			sorted_insert(list1->x, list_union);
+		}
+		else if (list2->next->x > list1->next->x) {
+			list2 = list2->next;
+			sorted_insert(list2->x, list_union);
+		}
+	}
+	while (list1->next != NULL) {
+		list1 = list1->next;
+		sorted_insert(list1->x, list_union);
+	}
+	while (list2->next != NULL) {
+		list2 = list2->next;
+		sorted_insert(list2->x, list_union);
+	}
+	printf("Unija je: ");
+	show_specific(list_union);
+}
+
+void make_intersection(position list1, position list2) {
+	position list_intersection = (position)malloc(sizeof(struct list));
+	list_intersection->next = NULL;
+
+	while (list1->next != NULL && list2->next != NULL) {
+		if (list1->next->x == list2->next->x) {
+			list1 = list1->next;
+			list2 = list2->next;
+			sorted_insert(list1->x, list_intersection);
+		}
+		else if (list1->next->x > list2->next->x) {
+			list1 = list1->next;
+		}
+		else if (list2->next->x > list1->next->x) {
+			list2 = list2->next;
+		}
+	}
+	printf("Presjek je:");
+	show_specific(list_intersection);
 }
