ddac2e4783966e5a1c20d6f6e9c54437702e4259
Luka Baranovic <73253333+LukaBaranovic@users.noreply.github.com>
2021-01-07 16:51:56 +0100

    Update getSentiment.h

--- Praktični rad/Sentiment analysis using Trie data structure/getSentiment.h
+++ Praktični rad/Sentiment analysis using Trie data structure/getSentiment.h
@@ -5,8 +5,12 @@ Header file za racunanje sentimenta.
 #pragma once
 
 #include <stdio.h>
+#include <stdbool.h>
 #include "declaration.h"
 
 
 int compare(position);
+char* is_interpunction_first(char*);
+bool is_interpunction_last(position, int, char* );
 void calculate_sentiment(position);
+float sigmoid(int);
