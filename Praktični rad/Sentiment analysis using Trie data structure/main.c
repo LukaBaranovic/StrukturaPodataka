cc6fc1f99be98cccb5110c86ae5bc8582edd017a
Luka Baranovic <73253333+LukaBaranovic@users.noreply.github.com>
2021-01-11 18:54:55 +0100

    Update main.c

--- Praktični rad/Sentiment analysis using Trie data structure/main.c
+++ Praktični rad/Sentiment analysis using Trie data structure/main.c
@@ -1,16 +1,21 @@
 /*
 Main source file.
 
-U RECENICAMA/RIJECIMA SE MOGU KORISTITI I VELIKA I MALA SLOVA I INTERPUNKCIJSKI ZNAKOVI (prije i poslije rijeci)
+U RECENICAMA/RIJECIMA SE MOGU KORISTITI I VELIKA I MALA SLOVA I INTERPUNKCIJSKI ZNAKOVI (prije i poslije rijeci).
 
-U datoteci predifiniramo vrijednost sentimenta odredenih rijeci koji program iscitaje i sprema u 'Trie'.
-Zatim nas program pita da unesemo recenicu iz koje racuna vrijednost sentimenta s obzirom na rijeci.
-Vrijednosti mogu bit pozitivne (+) za pozitvne rijeci, negativne (-) za negativne rijeci, a neutralne (0)
-za sve rijeci koje nisu predifinirane u datoteci.
+U datoteci unesemo rijec i njezin sentiment (dobaer 1) te ako sentiment u rangu <-1,1> onda nam program to kaze i 
+ne ukljuci tu rijec u trie.
 */
 
 #include <stdio.h>
 #include "declaration.h"
+
+/*
+Funckija: main
+Poziva funkciju run_program za pokretanje programa.
+Return: /
+*/
+
 
 int main() {
 	run_program();
