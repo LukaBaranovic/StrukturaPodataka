fa4db70b191755bceebf69b1614b0d81a2b9870e
Luka Baranovic <73253333+LukaBaranovic@users.noreply.github.com>
2021-01-19 20:13:13 +0100

    Update main.c

--- Zadatak 9/main.c
+++ Zadatak 9/main.c
@@ -2,206 +2,135 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+#define BUFFER 256
 
 typedef struct Node* position;
-
 struct Node {
 	int Number;
-	position Left, Right;
+	char Operand;
+	position Left, Right, Next;
 };
 
 position initialize();
-void displayOptions();
-void printDifference(int);
-position appendNew(position);
-position append(position, int);
-void search(position);
-position find(position, int);
-position findMinimum(position);
-position findMaximum(position);
-void deleteAll(position);
-void display(position, int);
-void doOption(position);
-position deleteSpecific(position);
-position delete(position, int);
+position readFromFile();
+position separate(char*);
+bool isOperand(char);
+void pushNumber(position, int);
+void pushOperand(position, char);
+position popNumber(position);
+void infixToFile(position);
+void printToFile(position, FILE*);
 void start();
-
 
 int main() {
 	start();
 }
 
-
 void start() {
-	position main = initialize();
-	doOption(main);
-}
-
-position delete(position head, int number) {
-	if (head == NULL) return NULL;
-	if (head->Number < number)
-		head->Right = delete(head->Right, number);
-	else
-		head->Left = delete(head->Left, number);
-	if (head->Number == number) {
-		if (head->Right == NULL) {
-			position tmp = head->Left;
-			free(head);
-			return tmp;
-		}
-		else if (head->Left == NULL) {
-			position tmp = head->Right;
-			free(head);
-			return tmp;
-		}
-		else {
-			position tmp = findMinimum(head->Right);
-			head->Number = tmp->Number;
-			head->Right = delete(head->Right, tmp->Number);
-		}
-	}
-	return head;
-}
-
-position deleteSpecific(position head) {
-	int number = 0;
-	printf("Insert element to delete: ");
-	scanf(" %d", &number);
-	return delete(head, number);
-}
-
-void doOption(position head) {
-	int option = 0, loop = 1;
-
-	while (loop == 1) {
-		displayOptions();
-		printf("Chose an option: ");
-		scanf("%d", &option);
-
-		switch (option) {
-		case 1:
-			head = appendNew(head);
-			break;
-		case 2:
-			display(head->Right, 0);
-			break;
-		case 3:
-			search(head->Right);
-			break;
-		case 4:
-			deleteSpecific(head);
-			break;
-		case 0:
-			loop = 0;
-			break;
-		default:
-			break;
-		}
-	}
-}
-
-
-void display(position head, int difference) {
-	if (head == NULL) return;
-	printf(" %d \n", head->Number);
-	if (NULL != head->Left) {
-		printDifference(difference);
-		printf("Left part:  ");
-		display(head->Left, difference + 2);
-	}
-	if (NULL != head->Right) {
-		printDifference(difference);
-		printf("Right part: ");
-		display(head->Right, difference + 2);
-	}
-}
-
-
-void deleteAll(position head) {
-	if (head == NULL) return 0;
-	deleteAll(head->Left);
-	deleteAll(head->Right);
-	free(head);
-}
-
-position findMinimum(position head) {
-	if (head->Left != NULL) return findMinimum(head->Left);
-	return head;
-}
-
-position findMaximum(position head) {
-	if (head->Right != NULL) return findMaximum(head->Right);
-	return head;
-}
-
-
-void search(position head) {
-	int number = 0;
-	printf("Insert a number to search for:  ");
-	scanf(" %d", &number);
-
-	if (find(head, number) == NULL)
-		printf("Element does not exist! \n");
-	else
-		printf("Element does exist! \n");
-}
-
-position find(position head, int number) {
-	if (head == NULL) return NULL;
-	if (head->Number == number) return head;
-	if (head->Right < number)
-		return find(head->Right, number);
-	else
-		return find(head->Left, number);
-	return head;
-}
-
-position append(position head, int number) {
-	if (NULL == head) {
-		head = initialize();
-		head->Number = number;
-		return head;
-	}
-	else {
-		if (head->Number == number)
-			return NULL;
-		else if (head->Number < number)
-			head->Right = append(head->Right, number);
-		else if (head->Number > number)
-			head->Left = append(head->Left, number);
-	}
-	return head;
-}
-
-position appendNew(position head) {
-	int number = 0;
-	printf("Insert new element: ");
-	scanf(" %d", &number);
-	return append(head, number);
-}
-
-void displayOptions() {
-	printf("(1) Insert element \n");
-	printf("(2) Display element/elements \n");
-	printf("(3) Search for element \n");
-	printf("(4) Delete element \n");
-	printf("(0) Exit program \n");
+	position main = readFromFile();
+	infixToFile(main);
 }
 
 position initialize() {
 	position new = (position)malloc(sizeof(struct Node));
-	if (new == NULL) {
-		printf("Malloc failed! \n");
-		return NULL;
-	}
-	new->Left = NULL;
+	if (new == NULL)  return NULL;
 	new->Right = NULL;
+	new->Left = NULL;
+	new->Next = NULL;
 	return new;
 }
 
-void printDifference(int n) {
-	int i;
-	for (i = 0; i < n; i++) {
-		printf(" ");
+position readFromFile() {
+	FILE* fp = fopen("postfix.txt", "r");
+	if (fp == NULL) return NULL;
+
+	char postfix[BUFFER];
+	fgets(postfix, BUFFER, fp);
+
+	fclose(fp);
+	return separate(postfix);
+}
+
+position separate(char* postfix) {
+	position head = initialize();
+	int number = 0;
+	int offset = 0;
+	int i = 0;
+
+	while (sscanf(postfix, "%d%n ", &number, &offset) > 0) {
+		postfix += offset;
+		pushNumber(head, number);
+		i = 0;
+		while (postfix[i] != '\0' && !isdigit(postfix[i])) {
+			if (isOperand(postfix[i]))
+				pushOperand(head, postfix[i]);
+			i++;
+		}
+		postfix += i;
 	}
+	return head;
+}
+
+bool isOperand(char operand) {
+	if (operand >= 40 && operand <= 47)
+		return true;
+	else
+		return false;
+}
+
+void pushNumber(position head, int number) {
+	if (head == NULL) return;
+	position tmp = initialize();
+	if (tmp == NULL) return;
+
+	tmp->Number = number;
+	tmp->Operand = '\0';
+
+	tmp->Next = head->Next;
+	head->Next = tmp;
+}
+
+void pushOperand(position head, char operand) {
+	if (head == NULL) return;
+	position tmp = initialize();
+	if (tmp == NULL) return;
+
+	tmp->Right = popNumber(head);
+	tmp->Left = popNumber(head);
+	tmp->Operand = operand;
+
+	tmp->Next = head->Next;
+	head->Next = tmp;
+}
+
+position popNumber(position head) {
+	if (head == NULL || head->Next == NULL) return NULL;
+	position tmp = head->Next;
+	head->Next = head->Next->Next;
+	tmp->Next = NULL;
+	return tmp;
+}
+
+void infixToFile(position head) {
+	if (head == NULL) return;
+	FILE* fp = fopen("infix.txt", "w");
+	if (fp == NULL) return;
+
+	printToFile(head->Next, fp);
+
+	fclose(fp);
+}
+
+void printToFile(position head, FILE* fp) {
+	if (head == NULL) return;
+	printToFile(head->Left, fp);
+
+	if (isOperand(head->Operand))
+		fprintf(fp, " %c ", head->Operand);
+	else
+		fprintf(fp, " %d ", head->Number);
+
+	printToFile(head->Right, fp);
 }
